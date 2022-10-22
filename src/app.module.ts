6290acc24eb5c6777503e20d3abf679a55f167b2
Anel <anelgabriel002@gmail.com>
2022-10-22 13:46:06 -0400

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,54 @@
+import { join } from 'path';
+
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
+import { FilesModule } from './files/files.module';
+import { SeedModule } from './seed/seed.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..','public'),
+    }),
+
+    ProductsModule,
+
+
+    CommonModule,
+
+
+    FilesModule,
+
+
+    SeedModule,
+
+
+    AuthModule
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
